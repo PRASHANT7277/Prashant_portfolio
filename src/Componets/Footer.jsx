916ec9ca9012d938683a2f4f81656f694c723df8
PRASHANT7277 <KUMAR.PRASHANT477@GMAIL.COM>
2022-09-13 11:59:00 +0530

    footer

--- src/Componets/Footer.jsx
+++ src/Componets/Footer.jsx
@@ -12,12 +12,7 @@ const Footer = () => {
     <>
 
 <footer style={{backgroundColor:"white",color:"black"}}>
-      {/* <div className="media-icons" style={{color:"pink"}}>
-          <a href="https://www.linkedin.com/in/pooja-dhuri-a0b63b221/" target="_blank"
-            ><i className="fab fa-linkedin" id="icons"></i></a>
-          <a href="https://github.com/PoojaD12345"target="_blank"
-            ><i className="fab fa-github" id='icons'></i></a>
-        </div> */}
+      
       <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><BsChevronDoubleUp style={{height:"40px",width:"40px",marginBottom:"-0px",position:"sticky"}}/></a></div>
 </footer>
     
