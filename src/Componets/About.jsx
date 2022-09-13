611bc6d70ffc40630b31f2adb65972c1e9e2f2ed
PRASHANT7277 <KUMAR.PRASHANT477@GMAIL.COM>
2022-09-14 00:36:38 +0530

    aa

--- src/Componets/About.jsx
+++ src/Componets/About.jsx
@@ -15,8 +15,8 @@ const About = () => {
             <div className="right">
               <div className="topic">I like to code because it's a usable <span style={{color:"#9b1750"}}>ART</span></div>
               <p>
-                Hello World! I'm a passinate  Developer.I love to cook and always want to be updated on the world and domestic politics. Currently, looking forward to joining a company where I
-                will be able to explore My skills. I love to combine the worlds of logic and creative design to make eye-catching,accessible,and user-friendly websites and applications.
+                Hello World! I'm a passinate  Developer.I love to cook and always want to be updated on the world and domestic politics. Currently, looking forward to joining a company where I will
+                be able to explore My skills. I love to combine the worlds of logic and creative design to make eye-catching,accessible,and user-friendly websites and applications.
               </p>
             </div>
           </div>
--- src/Componets/Home.jsx
+++ src/Componets/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Typical from 'react-typical'
 import a from "./Images/a.jpg"
 const Home = () => {
   return (
@@ -9,7 +10,14 @@ const Home = () => {
         <div className="text">
           <div className="text-one">Hello,my name is</div>
           <div className="text-two">Prashant kumar</div>
-          <div className="text-three">I'm a <span style={{color:"#9b1750"}}>Full Stack Developer</span> </div>
+          <div className="text-three">I'm a <span style={{color:"blueviolet"}}>
+            <Typical
+              steps={['',200,'Full Stack Developer!',2000]}
+               loop={Infinity}
+               wrapper="p"
+            /></span> </div>
+          
+
           <div className="button">
                 <button>
                   <a href="https://drive.google.com/file/d/1YqCzHtQiOGUWOWtRVgo4d9H8i6lXI0VY/view?usp=sharing" target="_blank">
