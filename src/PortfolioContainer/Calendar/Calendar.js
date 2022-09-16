e53384c7ddc85a009178168a3976fac62c430a0d
PRASHANT7277 <KUMAR.PRASHANT477@GMAIL.COM>
2022-09-16 22:59:33 +0530

    change

--- src/PortfolioContainer/Calendar/Calendar.js
+++ src/PortfolioContainer/Calendar/Calendar.js
@@ -5,7 +5,7 @@ import { Row } from "react-bootstrap";
 function Calendar() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
+      <h1 className="project-heading" style={{ paddingBottom: "0px" }}>
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
