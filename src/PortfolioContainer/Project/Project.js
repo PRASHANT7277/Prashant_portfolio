eff88412f0bf633589528da059df7712006f1987
PRASHANT7277 <KUMAR.PRASHANT477@GMAIL.COM>
2022-11-28 01:02:04 +0530

    project update

--- src/PortfolioContainer/Project/Project.js
+++ src/PortfolioContainer/Project/Project.js
@@ -1,23 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import './Project.css';
-import  Hellobonsai from '../../assets/pro.jpg'
-import TannerGoods from '../../assets/tanner.png';
-import Bellavita from '../../assets/bellavita.png';
-import Kimaye from '../../assets/kimaye.png';
-import chakra from '../../assets/chakra.png'
-import react from '../../assets/react.png'
-import express from '../../assets/express.jpg'
-import node from '../../assets/nod.jpg'
-import mongo from '../../assets/mongo.jpg'
-import {AiOutlineGithub} from 'react-icons/ai';
-import {TbExternalLink} from 'react-icons/tb';
-import html5 from '../../assets/icons8-html-5-144.png';
-import css3 from '../../assets/icons8-css3-144.png';
-import js from '../../assets/icons8-javascript-144.png';
+import React, { useState, useEffect } from "react";
+import "./Project.css";
+import Hellobonsai from "../../assets/pro.jpg";
+import TannerGoods from "../../assets/tanner.png";
+import Bellavita from "../../assets/bellavita.png";
+import Kimaye from "../../assets/kimaye.png";
+import chakra from "../../assets/chakra.png";
+import react from "../../assets/react.png";
+import express from "../../assets/express.jpg";
+import node from "../../assets/nod.jpg";
+import mongo from "../../assets/mongo.jpg";
+import next from "../../assets/next.png";
+import redux from "../../assets/redux.png";
+import jwt from "../../assets/jwt.png";
+import take from "../../assets/take.png";
+import { AiOutlineGithub } from "react-icons/ai";
+import { TbExternalLink } from "react-icons/tb";
+import html5 from "../../assets/icons8-html-5-144.png";
+import css3 from "../../assets/icons8-css3-144.png";
+import js from "../../assets/icons8-javascript-144.png";
 
 export default function Project() {
-    
-
   return (
     <div className="project-container" id="Projects">
       <div className="project-header">
@@ -31,6 +33,43 @@ export default function Project() {
       <div className="projectDetails">
         <div className="projectCard">
           {/* card */}
+          <div className="project-card">
+            <img id="spe" src={take} alt="" />
+            <div className="card-body">
+              <div class="badge">BACKEND & FRONTEND</div>
+              <div className="projectName">Zara Clone[Group Project]</div>
+              <div className="techStack">
+                <img src={next} />
+                <img src={redux} />
+                <img src={jwt} />
+                <img src={mongo} />
+                <img src={chakra} />
+                <img src={js} />
+              </div>
+              <div className="contentProject">
+                ZARA is a Spanish multi-national retail clothing chain. It
+                specialises in fast fashion, and sells clothing, accessories,
+                shoes, beauty products and perfumes.
+              </div>
+              <div className="projectLinks">
+                <a
+                  className="projectbtn1"
+                  href="https://github.com/Surya9263/flashy-burst-4121"
+                  target="_blank"
+                >
+                  <AiOutlineGithub /> GitHub
+                </a>
+                <a
+                  className="projectbtn2"
+                  href="https://takeitnow.vercel.app/"
+                  target="_blank"
+                >
+                  Demo <TbExternalLink />
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* card2 */}
           <div className="project-card">
             <img id="spe" src={Hellobonsai} alt="" />
             <div className="card-body">
@@ -69,9 +108,9 @@ export default function Project() {
             </div>
           </div>
 
-          {/* card 2 */}
+          {/* card 3 */}
           <div className="project-card">
-            <img id="spec"  src={TannerGoods} alt="" />
+            <img id="spec" src={TannerGoods} alt="" />
             <div className="card-body">
               <div class="badge">Frontend</div>
               <div className="projectName">Tanner Goods clone</div>
@@ -106,45 +145,9 @@ export default function Project() {
             </div>
           </div>
 
-          {/* card 3 */}
+          {/* card 4 */}
           <div className="project-card">
-            <img src={Bellavita} alt="" />
-            <div className="card-body">
-              <div class="badge">Frontend</div>
-              <div className="projectName">Bella Vita Organics Clone</div>
-              <div className="techStack">
-                <img src={react} />
-                <img src={chakra} />
-                <img src={html5} />
-                <img src={css3} />
-                <img src={js} />
-              </div>
-              <div className="contentProject">
-                This is a clone of E-commerce website where we offers Ayurvedic
-                & Natural Beauty Care Products made from 100% Natural
-                Ingredients..
-              </div>
-              <div className="projectLinks">
-                <a
-                  className="projectbtn1"
-                  href="https://github.com/PRASHANT7277/-breakable-bikes-2790"
-                  target="_blank"
-                >
-                  <AiOutlineGithub /> GitHub
-                </a>
-                <a
-                  className="projectbtn2"
-                  href="https://bellavita-alpha.vercel.app/"
-                  target="_blank"
-                >
-                  Demo <TbExternalLink />
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* card 1 */}
-          <div className="project-card">
-            <img src={Kimaye} alt="" />
+            <img id="spec" src={Kimaye} alt="" />
             <div className="card-body">
               <div class="badge">FRONTEND</div>
               <div className="projectName">Kimaye clone[Group Project]</div>
